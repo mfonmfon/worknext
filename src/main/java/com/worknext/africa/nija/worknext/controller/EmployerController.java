9ccfc4dff037a299530bd25bbad3277d060d8269
mfonmfon <mfonm579@gmail.com>
2024-12-04 15:30:42 +0100

    commited here

--- src/main/java/com/worknext/africa/nija/worknext/controller/EmployerController.java
+++ src/main/java/com/worknext/africa/nija/worknext/controller/EmployerController.java
@@ -5,12 +5,15 @@ import com.worknext.africa.nija.worknext.Dtos.request.EmployerRegistrationReques
 import com.worknext.africa.nija.worknext.Dtos.request.LoginUserRequest;
 import com.worknext.africa.nija.worknext.Dtos.request.UpLoadPostRequest;
 import com.worknext.africa.nija.worknext.Dtos.response.*;
+import com.worknext.africa.nija.worknext.data.model.Employer;
 import com.worknext.africa.nija.worknext.exceptions.EmployersNotFoundException;
 import com.worknext.africa.nija.worknext.service.interfaces.EmployersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/employers")
 @RestController
@@ -47,24 +50,85 @@ public class EmployerController {
             return new ResponseEntity<>(new ApiResponse(false, exception.getMessage()), HttpStatus.BAD_REQUEST);
         }
     }
-    @PutMapping("/editProfile")
+    @PatchMapping("/editProfile")  //this endpoint is responding but is not changing the state of the fields yet int the db
     public ResponseEntity<?> editProfile(@RequestBody EditProfileRequest editProfileRequest){
         try{
             EditProfileResponse editProfileResponse = employerService.editProfile(editProfileRequest);
             return new ResponseEntity<>(new ApiResponse(true, editProfileResponse), HttpStatus.OK);
-        } catch (EmployersNotFoundException e) {
-            return new ResponseEntity<>(new ApiResponse(false, e.getMessage()), HttpStatus.NOT_FOUND);
+        } catch (EmployersNotFoundException exception) {
+            return new ResponseEntity<>(new ApiResponse(false, exception.getMessage()), HttpStatus.NOT_FOUND);
         }
     }
 
-    @DeleteMapping("/deletePost/{jobPostId}")
+    @DeleteMapping("/deleteAccount/{jobPostId}")
     public ResponseEntity<?> deleteAccount(@PathVariable Long jobPostId){
         try{
             DeleteUserResponse deleteUserResponse = employerService.deleteEmployer(jobPostId);
             return new ResponseEntity<>(new ApiResponse(true, deleteUserResponse), HttpStatus.OK);
         }
         catch(Exception exception){
-            return new ResponseEntity<>(new ApiResponse(false, exception.getMessage()), HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(new ApiResponse(false, exception.getMessage()), HttpStatus.BAD_REQUEST);
         }
+    }
+    @GetMapping
+    public ResponseEntity<?> getAllEmployers(){
+       try {
+           List<Employer> getAllEmployers =employerService.getAllEmployers();
+           return new ResponseEntity<>(new ApiResponse(true, getAllEmployers), HttpStatus.OK);
+       }
+       catch(Exception exception){
+           return new ResponseEntity<>(new ApiResponse(false, exception.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/getEmployersByCompanyName/{companyName}")
+    public ResponseEntity<?> getEmployersByCompanyName(@PathVariable String companyName){
+        try{
+            List<Employer> findEmployerByCompanyName = employerService.getEmployersByCompanyName(companyName);
+            return new ResponseEntity<>(new ApiResponse(true, findEmployerByCompanyName), HttpStatus.OK);
+        }
+        catch(Exception exception){
+            return new ResponseEntity<>(new ApiResponse(false, exception.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+    }
+    @GetMapping("/getEmployerByCompanyLocation")
+    public ResponseEntity<?> getEmployerByCompanyLocation(@RequestParam String companyLocation){
+        try{
+            List<Employer> findEmployerByCompanyLocation = employerService.getEmployersByCompanyLocation(companyLocation);
+            return new ResponseEntity<>(new ApiResponse(true, findEmployerByCompanyLocation), HttpStatus.OK);
+        }
+        catch(Exception exception){
+            return new ResponseEntity<>(new ApiResponse(false, exception.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+    }
+    @GetMapping("/getEmployerByCompanyDescription")
+    public ResponseEntity<?> getEmployerByCompanyDescription(@RequestParam String companyDescription){
+        try{
+            List<Employer> findEmployerByCompanyDescription = employerService.getEmployersByCompanyDescription(companyDescription);
+            return new ResponseEntity<>(new ApiResponse(true, findEmployerByCompanyDescription), HttpStatus.OK);
+        }
+        catch(Exception exception){
+            return new ResponseEntity<>(new ApiResponse(false, exception.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+    }
+    @GetMapping("/getEmployerByEmail")
+    public ResponseEntity<?> getEmployerByEmail(@RequestParam String email){
+        try{
+            List<Employer> findEmployerByEmail = employerService.getEmployersByEmail(email);
+            return new ResponseEntity<>(new ApiResponse(true, findEmployerByEmail), HttpStatus.OK);
+        }
+        catch(Exception exception){
+            return new ResponseEntity<>(new ApiResponse(false, exception.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+    }
+    @GetMapping("/getEmployerById")
+    public ResponseEntity<?> getEmployerById(@RequestParam Long employerId){
+       try{
+             employerService.getEmployersById(employerId);
+           return new ResponseEntity<>(new ApiResponse(true, "Employer found"), HttpStatus.OK);
+       }
+       catch(EmployersNotFoundException exception){
+           return new ResponseEntity<>(new ApiResponse(false, exception.getMessage()), HttpStatus.NOT_FOUND);
+       }
     }
 }
--- src/main/java/com/worknext/africa/nija/worknext/service/implimentation/EmployersServiceImpl.java
+++ src/main/java/com/worknext/africa/nija/worknext/service/implimentation/EmployersServiceImpl.java
@@ -86,8 +86,8 @@ public class EmployersServiceImpl implements EmployersService {
     public EditProfileResponse editProfile(EditProfileRequest editProfileRequest) throws EmployersNotFoundException {
         Employer employer = employersRepository.findById(editProfileRequest.getEmployerId())
                 .orElseThrow(()-> new EmployersNotFoundException("Employer not found"));
-        modelMapper.map(editProfileRequest, Employer.class);
-        employersRepository.save(employer);
+        Employer employers = modelMapper.map(editProfileRequest, Employer.class);
+        employersRepository.save(employers);
         EditProfileResponse editProfileResponse = modelMapper.map(employer, EditProfileResponse.class);
         editProfileResponse.setMessage("Edited profile");
         return editProfileResponse;
